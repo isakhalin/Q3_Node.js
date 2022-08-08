a9bbedb57fdd4be5bbc4819e1a0a5c7759374b96
isakhalin <47768954+isakhalin@users.noreply.github.com>
2022-08-09 03:04:00 +1100

    some

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+// const file = fs.readFileSync("index.html", {
+//     encoding: "utf-8"
+// });
+//
+// console.log(file)
+//
+// fs.promises.readFile("index.html", ).then(console.log);
+
+fs.readFile("inde.html", (err, data) => {
+    if (err){
+        console.log("ERROR", err)
+    }
+    console.log(data)
+})
